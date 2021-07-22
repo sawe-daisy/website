04b9e50bef5c34441008278631ee965922a6b827
sawe-daisy <deesawe16@gmail.com>
2021-07-22 11:04:11 +0300

    website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
